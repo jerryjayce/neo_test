7bfa13f20557e04e3a25d06bde6c60ee1dc5389b
jerryjayce <jerryjayce@gmail.com>
2023-07-07 19:24:32 +0100

    rand changes

--- database/mysql/migrations/20230706115002-create-driver.js
+++ database/mysql/migrations/20230706115002-create-driver.js
@@ -2,7 +2,7 @@
 /** @type {import("sequelize-cli").Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("drivers", {
+        await queryInterface.createTable("vehicles", {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("drivers");
+        await queryInterface.dropTable("vehicles");
     }
 };
